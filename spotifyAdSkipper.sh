220978177b702947336925d04bece8c6337f322f
henryli17 <44710606+henryli17@users.noreply.github.com>
2021-01-23 01:49:13 +0000

    Update spotifyAdSkipper.sh

--- spotifyAdSkipper.sh
+++ spotifyAdSkipper.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+$macOsUsername = ""
 $spotifyUsername = ""
 $restartSpotifyAppleScriptDirectory = ""
 
-/usr/local/bin/fswatch -0 "~/Library/Application Support/Spotify/Users/${spotifyUsername}-user/ad-state-storage.bnk" | xargs -n1 -I{} osascript "$restartSpotifyAppleScriptDirectory"
+/usr/local/bin/fswatch -0 "/Users/${macOsUsername}/Library/Application Support/Spotify/Users/${spotifyUsername}-user/ad-state-storage.bnk" | xargs -n1 -I{} osascript "$restartSpotifyAppleScriptDirectory"
